35dbea66fca532cc92bb616796ef6c4f720dc287
danthek <abraham8507@gmail.com>
2022-10-03 19:29:07 -0500

    refactor code

--- src/testAutomation/TestRunner/TestRunner.java
+++ src/testAutomation/TestRunner/TestRunner.java
@@ -1,4 +1,5 @@
 package testAutomation.TestRunner;
+
 import org.testng.annotations.DataProvider;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
@@ -16,44 +17,37 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 }
 
 //Another test runner config
-/*package testAutomation.TestRunner;
-
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.CucumberFeatureWrapper;
-import cucumber.api.testng.PickleEventWrapper;
-import cucumber.api.testng.TestNGCucumberRunner;
-
-@CucumberOptions(features = "./features/", glue = { "stepDefinitions.StepDef"},  tags = { "@Login, @BadCreds, @withOutline" },plugin = {
-		"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber.json" }, monochrome = true)
-
-public class TestRunner {
-	private TestNGCucumberRunner testNGCucumberRunner;
-
-	@BeforeClass(alwaysRun = true)
-	public void setUpClass() throws Exception {
-		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-	}
-
-	@Test(dataProvider = "features")
-	public void feature(PickleEventWrapper eventwrapper, CucumberFeatureWrapper cucumberFeature) throws Throwable {
-		// testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-		testNGCucumberRunner.runScenario(eventwrapper.getPickleEvent());
-	}
-
-	@DataProvider // (parallel=true)
-	public Object[][] features() {
-		// return testNGCucumberRunner.provideFeatures();
-		return testNGCucumberRunner.provideScenarios();
-	}
-
-	@AfterClass(alwaysRun = true)
-	public void tearDownClass() throws Exception {
-		testNGCucumberRunner.finish();
-	}
-}*/
-
+/*
+ * package testAutomation.TestRunner;
+ * 
+ * import org.testng.annotations.AfterClass; import
+ * org.testng.annotations.BeforeClass; import
+ * org.testng.annotations.DataProvider; import org.testng.annotations.Test;
+ * 
+ * import cucumber.api.CucumberOptions; import
+ * cucumber.api.testng.CucumberFeatureWrapper; import
+ * cucumber.api.testng.PickleEventWrapper; import
+ * cucumber.api.testng.TestNGCucumberRunner;
+ * 
+ * @CucumberOptions(features = "./features/", glue = {
+ * "stepDefinitions.StepDef"}, tags = { "@Login, @BadCreds, @withOutline"
+ * },plugin = { "pretty", "html:target/site/cucumber-pretty",
+ * "json:target/cucumber.json" }, monochrome = true)
+ * 
+ * public class TestRunner { private TestNGCucumberRunner testNGCucumberRunner;
+ * 
+ * @BeforeClass(alwaysRun = true) public void setUpClass() throws Exception {
+ * testNGCucumberRunner = new TestNGCucumberRunner(this.getClass()); }
+ * 
+ * @Test(dataProvider = "features") public void feature(PickleEventWrapper
+ * eventwrapper, CucumberFeatureWrapper cucumberFeature) throws Throwable { //
+ * testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+ * testNGCucumberRunner.runScenario(eventwrapper.getPickleEvent()); }
+ * 
+ * @DataProvider // (parallel=true) public Object[][] features() { // return
+ * testNGCucumberRunner.provideFeatures(); return
+ * testNGCucumberRunner.provideScenarios(); }
+ * 
+ * @AfterClass(alwaysRun = true) public void tearDownClass() throws Exception {
+ * testNGCucumberRunner.finish(); } }
+ */
